739cecdbe53115ea1187eb54638170af76d18ec9
Hüseyin VURAL <huseyin@meditopia.com>
2023-03-20 11:33:58 +0300

    Add pair model commands.

--- DigiPrice/Screens/CurrencyList/Model/PairItem.swift
+++ DigiPrice/Screens/CurrencyList/Model/PairItem.swift
@@ -7,25 +7,43 @@
 
 import Foundation
 
+/// Model for holding basic data about a currency pair for display in the UI.
 final class PairDisplayItem {
+    /// The symbol representing the currency pair. It using item identification like id
     let symbol: String
+    /// The name of the currency pair in the format of "numeratorSymbol**/**denominatorSymbol".
     let name: String
+    /// The last traded price of the currency pair formatted as a string.
     let lastPrice: String
+    /// The trading volume of the currency pair formatted as a string.
     let volume: String
+    /// The percentage change in price over the last 24 hours formatted as a string.
     let dailyPercent: String
+    /// The symbol representing the denominator currency.
     let denominatorSymbol: String
+    /// The symbol representing the numerator currency.
     let numeratorSymbol: String
+    /// Whether the currency pair is currently rising in price.
     let isRising: Bool
+    /// Whether the currency pair is currently favorited by the user.
     var isFavorited: Bool
+    /// Whether this instance represents a placeholder for display in the UI.
     let isPlaceholder: Bool
     
+    /// The name of the color to use for displaying the daily percent value.
     var dailyPercentColorName: String {
         return isRising ? AppColors.highlightedPositiveColor.rawValue : AppColors.highlightedNegativeColor.rawValue
     }
+    /// The name of the color to use for displaying the favorite icon.
     var favoriteIconColorName: String {
         return isFavorited ? AppColors.favoritedColor.rawValue : AppColors.unfavoriteColor.rawValue
     }
     
+    /// Initializes a new PairDisplayItem with the given data and favorited status.
+    ///
+    /// - Parameters:
+    ///   - data: The data to use for initializing the PairDisplayItem.
+    ///   - isFavorited: Whether the currency pair is currently favorited by the user.
     init(data: PairResponseItem, isFavorited: Bool) {
         self.symbol = data.pair
         self.numeratorSymbol = data.numeratorSymbol
@@ -39,6 +57,10 @@ final class PairDisplayItem {
         self.isPlaceholder = false
     }
     
+    /// Initializes a new PairDisplayItem instance representing a placeholder for display in the UI.
+    ///
+    /// - Parameters:
+    ///   - isRising: Whether the placeholder should indicate a rising or falling price.
     init(isRising: Bool) {
         self.numeratorSymbol = "NumeratorName"
         self.name = "PairName"
